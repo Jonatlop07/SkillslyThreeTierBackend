b2b5521fcc66ada4669f4d81540f47769ae27335
Jonathan <jonatlop@gmail.com>
2021-11-03 19:19:50 -0500

    Setup of the file structure of the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -17,8 +17,21 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-extra-parens': 'warn',
+    '@typescript-eslint/no-extra-semi': 'error',
+    '@typescript-eslint/comma-spacing': 'error',
+    '@typescript-eslint/keyword-spacing': 'error',
+    '@typescript-eslint/brace-style': ['error', '1tbs']
   },
 };
