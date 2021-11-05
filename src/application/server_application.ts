c7c9e016f50260d1e98ca5d87f558215d6deb027
Jonathan <jonatlop@gmail.com>
2021-11-05 18:26:00 -0500

    Some changes

--- src/application/server_application.ts
+++ src/application/server_application.ts
@@ -65,7 +65,7 @@ export class ServerApplication {
       .build();
 
     const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api documentation', app, document);
+    SwaggerModule.setup('documentation', app, document);
   }
 
   public static new(): ServerApplication {
