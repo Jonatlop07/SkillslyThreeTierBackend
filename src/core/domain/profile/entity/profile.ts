7c4552d58f68a5530a5575044ad6a4d0074af2d5
Angel Gonzalez <angegonzalez@unal.edu.co>
2021-11-06 23:49:44 -0500

    Adding test cases following the BDD structure

---
+++ src/core/domain/profile/entity/profile.ts
@@ -0,0 +1,35 @@
+import { Entity } from '@core/common/entity/entity';
+import { CreateUserEntityPayload } from '@core/domain/user/entity/type/create_user_entity_payload';
+
+export class User extends Entity<number> {
+  private readonly _email: string;
+  private readonly _password: string;
+  private readonly _name: string;
+  private readonly _date_of_birth: string;
+
+  constructor(payload: CreateUserEntityPayload) {
+    super();
+    const { email, password, name, date_of_birth } = payload;
+    this._email = email;
+    this._password = password;
+    this._name = name;
+    this._date_of_birth = date_of_birth;
+    this.id = payload.id || 0;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get name (): string {
+    return this._name;
+  }
+
+  get date_of_birth (): string {
+    return this._date_of_birth;
+  }
+}
---
+++ src/core/domain/profile/entity/type/create_profile_entity_payload.ts
@@ -0,0 +1,8 @@
+export type CreateProfileEntityPayload = {
+  resume: string,
+  knowledge: Array<string>,
+  talents: Array<string>,
+  activities: Array<string>,
+  interests: Array<string>,
+  id?: number,
+};
