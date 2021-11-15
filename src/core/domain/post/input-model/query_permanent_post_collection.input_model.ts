1e72e98983514906fa8eda83805ebb0ee66cbb38
Jonathan <jonatlop@gmail.com>
2021-11-14 19:30:24 -0500

    Fixed conflicts with develop

--- src/core/domain/post/input-model/query_permanent_post_collection.input_model.ts
+++
@@ -1,3 +0,0 @@
-export default interface QueryPermanentPostCollectionInputModel {
-  user_id: string;
-}
--- src/core/domain/post/use-case/query_permanent_post.interactor.ts
+++
@@ -1,9 +0,0 @@
-import { Interactor } from '@core/common/use-case/interactor';
-import QueryPermanentPostInputModel from '../input-model/query_permanent_post.input_model';
-import QueryPermanentPostOutputModel from './output-model/query_permanent_post.output_model';
-
-export interface QueryPermanentPostInteractor
-  extends Interactor<
-  QueryPermanentPostInputModel,
-  QueryPermanentPostOutputModel
-  > {}
